e895ab1c66f2f280046f23266eb1540510565023
Matthew Huebert <no-email@gmail.com>
2016-01-10 21:21:00 +0100

    a bit more logging

--- src/mies_blank/core.cljs
+++ src/mies_blank/core.cljs
@@ -6,8 +6,6 @@
 
 (enable-console-print!)
 
-(println "Hello world!")
-
 (do
 
   (def test-compiler-opts {:eval       cljs/js-eval
@@ -15,16 +13,16 @@
                            :source-map true})
 
   (def test-cstate (cljs/empty-state))
-
   (def test-sample "(fn [] (let [x 7 y] (prn y)))")
 
-  (defn cb [s {:keys [value error] :as result}]
-    (prn "finished " s ": ")
+  (prn "Attempting to evaluate buggy code: " test-sample)
+
+  (defn cb [s {:keys [value error]}]
+    (prn "callback from " s ": ")
     (if error (prn "error: " error)
               (prn "value: " value))
     (when (= s :compile-str)
-      (js/eval value)))
+      (prn "Evaluating result of cljs/compile-str: " (js/eval value))))
 
   (cljs/eval-str test-cstate test-sample "eval-str" test-compiler-opts (partial cb :eval-str))
-
   (cljs/compile-str test-cstate test-sample "compile-str" test-compiler-opts (partial cb :compile-str)))
